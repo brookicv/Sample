4485728d7a391d8d3b6d27912cfb69f598560486
Brook <brookicv@gmail.com>
2016-10-26 18:30:04 +0800

    更新README，定义宏更改soundtouch的sample类型为16位有符号整数

--- README.md
+++ README.md
@@ -6,5 +6,8 @@
 
 - GifGenerator 使用多张bitmap图像生成Gif动画 Date:2016-9-28
 
+- SoundTouch 开源库SoundTouch的Visual Studio 2013版
+
 - wav_sound 使用FFmpeg的音频处理。
     - 将视频中的音频提取出来，并且保存为WAV文件。 Date:2016-10-21
+    - 使用SoundTouch库对音频进行变调、变声处理。 Date:2016-10-26
--- wav_sound/main.cpp
+++ wav_sound/main.cpp
@@ -5,8 +5,10 @@
 #include <cstdio>
 
 #include "MediaInfo.h"
+#include "..\SoundTouch\SoundTouch.h"
 
 using namespace std;
+
 
 int to_pcm()
 {
@@ -24,7 +26,7 @@ int to_pcm()
 
 	ofstream ofs("wildlife.wav", ofstream::binary);
 
-	AVSampleFormat dst_format = AV_SAMPLE_FMT_S16;
+	AVSampleFormat dst_format = AV_SAMPLE_FMT_S16; 
 	uint8_t dst_channels = 2;
 	auto dst_layout = av_get_default_channel_layout(dst_channels);
 
@@ -37,6 +39,22 @@ int to_pcm()
 		audio_ctx->channel_layout, audio_ctx->sample_fmt, audio_ctx->sample_rate, 0, nullptr);
 	if (!swr_ctx || swr_init(swr_ctx))
 		return -1;
+
+	////////////////////////////////////////////////////////////////////
+	// 设置SoundTouch，配置变调变速参数
+	soundtouch::SoundTouch s_touch;
+	s_touch.setSampleRate(audio_ctx->sample_rate); // 设置采样率
+	s_touch.setChannels(audio_ctx->channels); // 设置通道数
+
+	////////////////////////////////////////////
+	// 设置 rate或者pitch的改变参数
+	//s_touch.setRate(0.5); // 设置速度为0.5，原始的为1.0
+	//s_touch.setRateChange(-50.0);
+	s_touch.setPitch(0.1);
+	s_touch.setSetting(SETTING_USE_QUICKSEEK, 1); 
+	s_touch.setSetting(SETTING_USE_AA_FILTER, 1);
+
+	soundtouch::SAMPLETYPE touch_buffer[96000]; 
 
 	int pcm_data_size = 0;
 	while (av_read_frame(media.pFormatContext, packet) >= 0)
@@ -54,14 +72,47 @@ int to_pcm()
 
 			auto nb = swr_convert(swr_ctx, &buffer, 192000, (const uint8_t **)frame->data, frame->nb_samples);
 
-			auto length = nb * dst_channels * av_get_bytes_per_sample(dst_format);
-			ofs.write((char*)buffer, length);
+			//////////////////////////////////////////////////////////////
+			// 传入sample，并接收处理后的sample
 
-			pcm_data_size += length;
+			// 将解码后的buffer(uint8*)转换为soundtouch::SAMPLETYPE，也就是singed int 16
+			auto len = nb * dst_channels * av_get_bytes_per_sample(dst_format);
+			for (auto i = 0; i < len; i++)
+			{
+				touch_buffer[i] = (buffer[i * 2] | (buffer[i * 2 + 1] << 8));	
+			}
+
+			// 传入Sample
+			s_touch.putSamples(touch_buffer, nb);
+			do
+			{
+				// 接收处理后的sample
+				nb = s_touch.receiveSamples(touch_buffer, 96000);
+
+				auto length = nb * dst_channels * av_get_bytes_per_sample(dst_format);
+				ofs.write((char*)touch_buffer, length);
+
+				pcm_data_size += length;
+			} while (nb != 0);
 		}
 		av_packet_unref(packet);
 	}
 
+	///////////////////////////////////////////////
+	// 接收管道内余下的处理后数据
+	s_touch.flush();
+	int nSamples;
+	do
+	{
+		nSamples = s_touch.receiveSamples(touch_buffer, 96000);
+
+		auto length = nSamples * dst_channels * av_get_bytes_per_sample(dst_format);
+		ofs.write((char*)touch_buffer, length);
+
+		pcm_data_size += length;
+	} while (nSamples != 0);
+
+	// 写Wav文件头
 	Wave_header header(dst_channels, audio_ctx->sample_rate, av_get_bytes_per_sample(dst_format) * 8);
 	header.data->cb_size = ((pcm_data_size + 1) / 2) * 2;
 	header.riff->cb_size = 4 + 4 + header.fmt->cb_size + 4 + 4 + header.data->cb_size + 4;
@@ -82,39 +133,7 @@ int to_pcm()
 
 int main()
 {
-	av_register_all();
-
-	AVFormatContext *pFormat_context = nullptr;
-	AVOutputFormat *pOutput_format   = nullptr;
-
-	AVCodecContext *pCodec_context   = nullptr;
-	AVCodec *pCodec                  = nullptr;
-	AVStream *audio_st               = nullptr;
-	
-	pFormat_context = avformat_alloc_context();
-	char* out_file = "life.aac";
-	pOutput_format = av_guess_format(nullptr, out_file, nullptr);
-
-	pFormat_context->oformat = pOutput_format;
-
-	avio_open(&pFormat_context->pb, out_file, AVIO_FLAG_READ_WRITE);
-
-	audio_st = avformat_new_stream(pFormat_context, nullptr);
-
-	pCodec_context = audio_st->codec;
-
-	AVCodecParameters *codec_para = avcodec_parameters_alloc();
-	
-
-	pCodec_context->codec_id = pOutput_format->audio_codec;
-	pCodec_context->codec_type = AVMEDIA_TYPE_AUDIO;
-	pCodec_context->sample_fmt = AV_SAMPLE_FMT_S16;
-	pCodec_context->sample_rate = 44100;
-	pCodec_context->channel_layout = AV_CH_LAYOUT_STEREO;
-	pCodec_context->channels = av_get_channel_layout_nb_channels(pCodec_context->channel_layout);
-	pCodec_context->bit_rate = 64000;
-
-	av_dump_format(pFormat_context, 0, out_file, 1);
+	to_pcm();
 
 	cout << "end." << endl;
 	getchar();
